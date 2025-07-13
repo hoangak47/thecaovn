516247f4326919ebd97b50170b0755bed0457bc0
hoangak47 <hoang41911@gmail.com>
2025-07-13 14:27:03 +0700

    a

--- backend/app.js
+++ backend/app.js
@@ -17,10 +17,18 @@ const lienHeRoutes = require("./routes/lienHe.js");
 const app = express();
 app.use(express.json());
 
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",https://thecaovn.onrender.com/
+//     credentials: true, // nếu cần gửi cookie/auth (tùy)
+//   })
+// );
+
+// có thể dùng 2 link là http://localhost:3000 và https://thecaovn.onrender.com/
 app.use(
   cors({
-    origin: "http://localhost:3000", // cho phép frontend Next.js truy cập
-    credentials: true, // nếu cần gửi cookie/auth (tùy)
+    origin: ["http://localhost:3000", "https://thecaovn.onrender.com/"],
+    credentials: true,
   })
 );
 
