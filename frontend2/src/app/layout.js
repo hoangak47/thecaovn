aa913b1c2eda0fa5a78cba024d910532b0c57149
hoangak47 <hoang41911@gmail.com>
2025-10-09 20:46:40 +0700

    fix: move google site verification meta tag to the correct head section

--- frontend2/src/app/layout.js
+++ frontend2/src/app/layout.js
@@ -4,6 +4,12 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="vi">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="iDjEWJDFYYFHqH_2jK-C0jw75XTDyn608-pPwgSjSBg"
+        />
+      </head>
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <link
@@ -22,6 +28,11 @@ export default function RootLayout({ children }) {
           type="image/png"
           sizes="16x16"
           href="/favicon-16x16.png"
+        />
+
+        <meta
+          name="google-site-verification"
+          content="iDjEWJDFYYFHqH_2jK-C0jw75XTDyn608-pPwgSjSBg"
         />
       </Head>
       <body>{children}</body>
